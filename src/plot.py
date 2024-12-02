4b77157d3a8db6ab6de7421c8b752d90dabc6663
rcmkuin <r.c.m.kuin@lacdr.leidenuniv.nl>
2024-12-02 12:19:41 +0100

    "Update scripts from develop branch"

--- src/plot.py
+++ src/plot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -8,15 +9,33 @@ from dna_features_viewer import GraphicFeature, GraphicRecord
 
 from .mutation import Mutation
 from .sequence import Vector, Gene
-from .eblocks import EblockDesign
-
-# TODO Better parse vecotr information to plot and add a few tests
+from .DNABlocks import DNABlockDesign
 
 
 
 class Plot:
+    """
+    Plotting class for the DNABlocks design.
+
+    Parameters
+    ----------
+    DNABlocks_design_instance : DNABlockDesign
+        DNABlocks design instance.
+    mutation_instance : Mutation
+        Mutation instance.
+    vector_instance : Vector
+        Vector instance.
+    gene_instance : Gene
+        Gene instance.
+    output_dir : str
+        Output directory.
+    show : bool
+        Show plot.
+    save : bool
+        Save plot.
+    """
     def __init__(self, 
-                 eblocks_design_instance: EblockDesign,
+                 DNABlocks_design_instance: DNABlockDesign,
                  mutation_instance: Mutation,
                  vector_instance: Vector,
                  gene_instance: Gene,
@@ -25,7 +44,7 @@ class Plot:
                  show: bool = False,
                  save: bool = True):
         
-        self.eblocks_design_instance = eblocks_design_instance
+        self.DNABlocks_design_instance = DNABlocks_design_instance
         self.mutation_instance = mutation_instance
         self.vector_instance = vector_instance
         self.gene_instance = gene_instance
@@ -34,23 +53,19 @@ class Plot:
         self.save = save
 
     def save_plot(self, fig, filename, dpi=100, bbox_inches='tight', transparent=True):
-        """
-        Save plot to file.
-        """
+        """Save plot to file."""
         fig.savefig(os.path.join(self.output_dir, filename), dpi=dpi, bbox_inches=bbox_inches, transparent=transparent)
 
     def plot_histogram_mutations(self, figure_width=8, figure_length=5, filename='histogram_mutations.png'):
-        """
-        Show for every eBlock the number of mutations you can make.
-        """
-        counts = self.eblocks_design_instance.count_mutations_per_eblock()
+        """Show for every DNABlock the number of mutations you can make."""
+        counts = self.DNABlocks_design_instance.count_mutations_per_DNABlock()
         fig, ax = plt.subplots(figsize=(figure_width, figure_length))
         labels = list(counts.keys())
-        colors = list(self.eblocks_design_instance.eblock_colors.values())[:len(counts)]
+        colors = list(self.DNABlocks_design_instance.DNABlock_colors.values())[:len(counts)]
         ax.bar(range(len(counts)), list(counts.values()), align='center', color=colors)
         plt.xticks(range(len(counts)), labels, rotation=90)
-        ax.set_ylabel('Number of mutants per eBlock')
-        ax.set_title(f'Number of mutants per eBlock')
+        ax.set_ylabel('Number of mutants per DNABlock')
+        ax.set_title(f'Number of mutants per DNABlock')
         if self.save:
             self.save_plot(fig, filename)
         if self.show:
@@ -59,9 +74,7 @@ class Plot:
             plt.close()
         
     def plot_mutation_legend(self, legend_alpha=0.2, font_size='x-large', marker_size=10, linestyle='None', marker='o', loc='center', bbox_to_anchor=(0.5, 0.5)):
-        """
-        Legend for eBlocks plot
-        """
+        """Legend for DNABlocks plot."""
         # Create an empty plot with no data points
         fig, ax = plt.subplots(figsize=(3, 2))
         # Add mutation type colors to the legend
@@ -79,15 +92,12 @@ class Plot:
         if self.save:
             self.save_plot(fig, 'legend.png')
 
-    def plot_eblocks_mutations(self, 
-                               plot_eblocks=True, 
+    def plot_DNABlocks_mutations(self, 
+                               plot_DNABlocks=True, 
                                plot_mutations=True, 
                                figure_width=20, 
                                figure_length=10):
-        """
-        Plot mutations and selected eBlocks.
-        """
-
+        """Plot mutations and selected DNABlocks."""
         # Check if mutations and sequence are available
         if self.mutation_instance.mutations is None:
             raise ValueError("No mutations found. Please run the mutation class first.")
@@ -100,7 +110,6 @@ class Plot:
                                        strand=+1, 
                                        color=Vector(Gene).color, 
                                        label=f"{self.gene_instance.seqid}"))
-
         # Add mutations to plot
         if plot_mutations:
             for i in self.mutation_instance.silent_mutations:
@@ -126,14 +135,14 @@ class Plot:
                                                        color=self.mutation_instance.colors['Combined'], 
                                                        label=f"{mut.mutation[num]}"))
                         
-        # Add eBlocks to plot
-        if plot_eblocks:
-            for num, i in enumerate(self.eblocks_design_instance.wt_eblocks):
+        # Add DNABlocks to plot
+        if plot_DNABlocks:
+            for num, i in enumerate(self.DNABlocks_design_instance.wt_DNABlocks):
 
                 features.append(GraphicFeature(start=i.bin_start, 
                                                end=i.bin_end,
                                                strand=+1, 
-                                               color=self.eblocks_design_instance.eblock_colors[num + 1],
+                                               color=self.DNABlocks_design_instance.DNABlock_colors[num + 1],
                                                label=f"{i.name}"))
 
             
@@ -146,18 +155,16 @@ class Plot:
         else:
             plt.close()
 
-        if plot_eblocks and plot_mutations and self.save:
-            if self.eblocks_design_instance.cost_optimization:
-                self.save_plot(fig, f'eblocks_{self.gene_instance.seqid}_N{self.mutation_instance.n_mutants}_cost.png')
-            elif self.eblocks_design_instance.amount_optimization:
-                self.save_plot(fig, f'eblocks_{self.gene_instance.seqid}_N{self.mutation_instance.n_mutants}_amount.png')
-        if not plot_eblocks:
-            self.save_plot(fig, f'mutations_{self.gene_instance.seqid}_N{self.mutation_instance.n_mutants}_{self.eblocks_design_instance.optimization_method}.png')
+        if plot_DNABlocks and plot_mutations and self.save:
+            if self.DNABlocks_design_instance.cost_optimization:
+                self.save_plot(fig, f'DNABlocks_{self.gene_instance.seqid}_N{self.mutation_instance.n_mutants}_cost.png')
+            elif self.DNABlocks_design_instance.amount_optimization:
+                self.save_plot(fig, f'DNABlocks_{self.gene_instance.seqid}_N{self.mutation_instance.n_mutants}_amount.png')
+        if not plot_DNABlocks:
+            self.save_plot(fig, f'mutations_{self.gene_instance.seqid}_N{self.mutation_instance.n_mutants}_{self.DNABlocks_design_instance.optimization_method}.png')
 
     def extract_snapgene_features(self):
-        """
-        Extract features from a snapgene vector file and return them as a list of biotite sequence features.
-        """
+        """Extract features from a snapgene vector file and return them as a list of biotite sequence features."""
         if self.vector_instance.vector is None:
             raise ValueError("No vector found. Please run the sequence class first.")
 
@@ -194,9 +201,7 @@ class Plot:
         return vector_features
 
     def plot_vector(self, figsize=(8,8), fontsize=10):
-        """
-        Plot a plasmid map of the vector sequence.
-        """
+        """Plot a plasmid map of the vector sequence."""
         features = self.extract_snapgene_features()
         annotation = Annotation(features)
         fig = plt.figure(figsize=figsize)
--- src/primer.py
+++ src/primer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import primer3
 from Bio import SeqIO
@@ -6,11 +8,33 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation, CompoundLocation
 
 from .sequence import Vector
 from .mutation import Mutation
-from .eblocks import EblockDesign
+from .DNABlocks import DNABlockDesign
 
 
 
 class Primer:
+    """
+    This class contains the information of a primer
+    
+    Attributes:
+    ----------
+    name : str
+        Name of the primer
+    sequence_5to3 : str
+        Sequence of the primer in 5' to 3' direction
+    is_forward : bool
+        True if the primer is a forward primer
+    is_reverse : bool
+        True if the primer is a reverse primer
+    idx_start : int
+        Start index of the primer in the vector sequence
+    idx_end : int
+        End index of the primer in the vector sequence
+    tm : float
+        Melting temperature of the primer
+    gc_content : float
+        GC content of the primer
+    """
     def __init__(self,
                  name: str = None,
                  sequence_5to3: str = None,
@@ -32,9 +56,6 @@ class Primer:
 
     # @staticmethod
     # def check_multiple_binding_sites(vector: str, sequence: str):
-    #     # TODO Maybe use primerblast for this
-    #     # TODO Write some checks for this function
-    #     # TODO Check for exact cases where the sequence binds multiple times and almost exact cases where only few characters are different
     #     count = 0
     #     for i in range(len(vector) - len(sequence) + 1):
     #         sub = vector[i:i + len(sequence)]
@@ -49,11 +70,13 @@ class Primer:
 class Primer3Config:
     """
     This class contains the configuration for the primer3 design
+
+    Attributes:
+    ----------
+    PRIMER3_SETTINGS : dict
+        Contains the settings for the primer3 design
     """
 
-    # TODO Add some other parameters as well from https://primer3.org/manual.html
-    # TODO Add GC clamp parameter
-    
     @staticmethod
     def read_global_settings(file):
         """
@@ -97,10 +120,30 @@ class Primer3Config:
 class DesignPrimers:
     """
     This class designs primers to open-up the expression plasmid and Sequencing primers for validation. 
-    """
 
+    Attributes:
+    ----------
+    DNABlocks_design_instance : DNABlockDesign
+        Instance of the DNABlockDesign class
+    mutation_instance : Mutation
+        Instance of the Mutation class
+    vector_instance : Vector
+        Instance of the Vector class
+    primers_settingsfile : str
+        Path to the primer3 settings file
+    seqprimers_settingsfile : str
+        Path to the sequencing primer settings file
+    output_dir : str
+        Path to the output directory
+    minimum_overhang : int
+        Minimum overhang between primers and DNABlocks
+    void_length : int
+        Length of the void region for sequencing
+    verbose : bool
+        Print verbose output
+    """
     def __init__(self,
-                 eblocks_design_instance: EblockDesign,
+                 DNABlocks_design_instance: DNABlockDesign,
                  mutation_instance: Mutation,
                  vector_instance: Vector,
                  primers_settingsfile: str = None,
@@ -110,7 +153,7 @@ class DesignPrimers:
                  void_length: int = 400,
                  verbose: bool = True):
 
-        self.eblocks_design_instance = eblocks_design_instance
+        self.DNABlocks_design_instance = DNABlocks_design_instance
         self.mutation_instance = mutation_instance
         self.vector_instance = vector_instance
         self.primers_settingsfile = primers_settingsfile
@@ -121,18 +164,16 @@ class DesignPrimers:
         self.verbose = verbose
 
     def run_design(self):
-        """
-        Run the design of the primers
-        """
+        """Run the design of the primers."""
         min_oh = self.check_overlap()  # Check if the overlap is large enough
         
         print("Designing primer pairs ...")
-        primerpairs = self.run_design_primerpair()  # store primers: eblock.name : [fw_primer, rv_primer]
+        primerpairs = self.run_design_primerpair()  # store primers: DNABlock.name : [fw_primer, rv_primer]
 
         # Add primers to Genbank file of clones
-        for mut, eblock in self.eblocks_design_instance.eblocks.items():
+        for mut, DNABlock in self.DNABlocks_design_instance.DNABlocks.items():
             for k, v in primerpairs.items():
-                if k == eblock.name:
+                if k == DNABlock.name:
                     self.add_primers_to_genbank_file(genbank_file=os.path.join(self.output_dir, 'clones', f"{mut.name}", f"{mut.name}.gb"), primer=v[0])
                     self.add_primers_to_genbank_file(genbank_file=os.path.join(self.output_dir, 'clones', f"{mut.name}", f"{mut.name}.gb"), primer=v[1])
 
@@ -148,9 +189,9 @@ class DesignPrimers:
             self.primers_to_fasta(name=v[0].name, seq=v[0].sequence_5to3, directory=self.output_dir, filename='primers.fasta')
             self.primers_to_fasta(name=v[1].name, seq=v[1].sequence_5to3, directory=self.output_dir, filename='primers.fasta')
 
-        for mut, eblock in self.eblocks_design_instance.eblocks.items():
+        for mut, DNABlock in self.DNABlocks_design_instance.DNABlocks.items():
             for k, v in seqprimers.items():
-                if k == eblock.name:
+                if k == DNABlock.name:
                     self.add_primers_to_genbank_file(genbank_file=os.path.join(self.output_dir, 'clones', f"{mut.name}", f"{mut.name}.gb"), primer=v[0])
                     self.add_primers_to_genbank_file(genbank_file=os.path.join(self.output_dir, 'clones', f"{mut.name}", f"{mut.name}.gb"), primer=v[1])
         
@@ -158,30 +199,26 @@ class DesignPrimers:
 
         
     def run_design_seqprimer(self):
-        """
-        For each eBlock design a Forward and Reverse sequencing primer using primer3
-        """
-        primers = {}  # store primers: d[eblock.name] = [fw, rv]
-        for eblock in self.eblocks_design_instance.wt_eblocks:
-            start = self.vector_instance.circular_index(eblock.start_index - self.void_length, self.vector_instance.length)
+        """For each DNABlock design a Forward and Reverse sequencing primer using primer3."""
+        primers = {}  # store primers: d[DNABlock.name] = [fw, rv]
+        for DNABlock in self.DNABlocks_design_instance.wt_DNABlocks:
+            start = self.vector_instance.circular_index(DNABlock.start_index - self.void_length, self.vector_instance.length)
             length = self.void_length
             end = self.vector_instance.circular_index(start + self.void_length, self.vector_instance.length)
             if start > end:  # change the 0-point of the sequence
                 sequence = self.vector_instance.vector.seq[-1000:] + self.vector_instance.vector.seq[0:-1000]
-                idx_eblock = sequence.find(eblock.sequence)
-                start = self.vector_instance.circular_index(idx_eblock - self.void_length, self.vector_instance.length)
+                idx_DNABlock = sequence.find(DNABlock.sequence)
+                start = self.vector_instance.circular_index(idx_DNABlock - self.void_length, self.vector_instance.length)
                 length = self.void_length
-                # end = self.vector_instance.circular_index(start + 2*self.void_length, self.vector_instance.length)
             else:
                 sequence = self.vector_instance.vector.seq
 
             # Obtain FW primer
-            print("start", start, "length", length)
             result  = self.design_sequencing_primer(sequence=sequence, start=start, length=length)
-            fw_result = self.parse_primer3_result(result, eblock, type='seq', direction='forward')
-            primers[eblock.name] = [fw_result]
+            fw_result = self.parse_primer3_result(result, DNABlock, type='seq', direction='forward')
+            primers[DNABlock.name] = [fw_result]
             # Obtain RV primer
-            start = self.vector_instance.circular_index(eblock.end_index - self.void_length , self.vector_instance.length)
+            start = self.vector_instance.circular_index(DNABlock.end_index - self.void_length , self.vector_instance.length)
             length = self.void_length
             result  = self.design_sequencing_primer(sequence=self.vector_instance.vector.seq, start=start, length=length)
 
@@ -194,32 +231,32 @@ class DesignPrimers:
                     break
                 
             index = self.find_closest_higher_index(possible_starts, end)
-            rv_results = self.parse_primer3_result(result, eblock, type='seq', direction='reverse', index=index)
-            primers[eblock.name].append(rv_results)
+            rv_results = self.parse_primer3_result(result, DNABlock, type='seq', direction='reverse', index=index)
+            primers[DNABlock.name].append(rv_results)
         return primers
         
     def run_design_primerpair(self):
         primers = {}
-        for eblock in self.eblocks_design_instance.wt_eblocks:
+        for DNABlock in self.DNABlocks_design_instance.wt_DNABlocks:
 
-            mid_index = (eblock.start_index + eblock.end_index) // 2
-            length_product = len(self.vector_instance.vector.seq) - len(eblock.sequence)
+            mid_index = (DNABlock.start_index + DNABlock.end_index) // 2
+            length_product = len(self.vector_instance.vector.seq) - len(DNABlock.sequence)
             len_start = length_product - 75 # TODO Approximation
             len_end = length_product + 75
 
             sequence_template = self.vector_instance.vector.seq[mid_index:] + self.vector_instance.vector.seq[:mid_index]  # Linearize plasmid to allow for primer design
-            half_eblock = len(eblock.sequence) // 2
+            half_DNABlock = len(DNABlock.sequence) // 2
 
-            fw_end_range = self.vector_instance.circular_index(half_eblock - self.minimum_overhang, len(self.vector_instance.vector.seq))
-            fw_start_range = self.vector_instance.circular_index(half_eblock - self.eblocks_design_instance.min_overlap, len(self.vector_instance.vector.seq))
+            fw_end_range = self.vector_instance.circular_index(half_DNABlock - self.minimum_overhang, len(self.vector_instance.vector.seq))
+            fw_start_range = self.vector_instance.circular_index(half_DNABlock - self.DNABlocks_design_instance.min_overlap, len(self.vector_instance.vector.seq))
        
-            rv_start_range = self.vector_instance.circular_index(len(self.vector_instance.vector.seq) - half_eblock + self.minimum_overhang, len(self.vector_instance.vector.seq))
-            rv_end_range = self.vector_instance.circular_index(len(self.vector_instance.vector.seq) - half_eblock + self.eblocks_design_instance.min_overlap, len(self.vector_instance.vector.seq))  # TODO Check this range
+            rv_start_range = self.vector_instance.circular_index(len(self.vector_instance.vector.seq) - half_DNABlock + self.minimum_overhang, len(self.vector_instance.vector.seq))
+            rv_end_range = self.vector_instance.circular_index(len(self.vector_instance.vector.seq) - half_DNABlock + self.DNABlocks_design_instance.min_overlap, len(self.vector_instance.vector.seq))  # TODO Check this range
 
             result = self.find_primerpair(sequence_template, fw_start_range, fw_end_range, rv_start_range, rv_end_range, len_start, len_end)
-            fw, rv = self.parse_primer3_result(result, eblock, type='pair')
+            fw, rv = self.parse_primer3_result(result, DNABlock, type='pair')
 
-            primers[eblock.name] = [fw, rv]
+            primers[DNABlock.name] = [fw, rv]
 
         return primers
 
@@ -227,7 +264,6 @@ class DesignPrimers:
         try:
             result = primer3.bindings.design_primers(seq_args=Primer3Config.seq_args_sequencing_primer(sequence, start, length),
                                                      global_args=Primer3Config.read_global_settings(self.seqprimers_settingsfile))
-            print(result)
             # Check if a sequencing primer was found
             if 'PRIMER_LEFT_0_SEQUENCE' in result:
                 seqprimer = result['PRIMER_LEFT_0_SEQUENCE']
@@ -258,39 +294,35 @@ class DesignPrimers:
                 else:
                     pass
 
-    # def calculate_length_overhang(self, eblock, fw, rv):
+    # def calculate_length_overhang(self, DNABlock, fw, rv):
     #     """
-    #     Calculate the length of the overlap between the eblock and linearized plasmid
+    #     Calculate the length of the overlap between the DNABlock and linearized plasmid
     #     """
-    #     # TODO make this function
-    #     # TODO Take into account circular indexes of the plasmid
     #     # Forward primer
-    #     idx_eblock_end = eblock.end_index
+    #     idx_DNABlock_end = DNABlock.end_index
     #     idx_start_fw = fw.idx_start
-    #     len_oh_fw = idx_eblock_end - idx_start_fw
+    #     len_oh_fw = idx_DNABlock_end - idx_start_fw
 
     #     # Reverse primer
-    #     idx_eblock_start = eblock.start_index
+    #     idx_DNABlock_start = DNABlock.start_index
     #     idx_end_rv = rv.idx_end
-    #     len_oh_rv = idx_eblock_start - idx_end_rv
+    #     len_oh_rv = idx_DNABlock_start - idx_end_rv
 
     #     return len_oh_fw, len_oh_rv
     
     def primers_to_fasta(self, name, seq, directory, filename='primers.fasta'):
-        """
-        This function converts the primers to features that can be read by SnapGene.
-        """
+        """This function converts the primers to features that can be read by SnapGene."""
         self.make_fasta_file(directory, filename, header=False)
         with open(os.path.join(self.output_dir, filename), 'a') as f:
             f.write(f">{name}\n")
             f.write(f"{seq}\n")
 
-    def parse_primer3_result(self, primer3output, eblock, type, index=0, direction=None,):
+    def parse_primer3_result(self, primer3output, DNABlock, type, index=0, direction=None,):
         if type == "pair":
             # Forward (left) primer
             start_idx, end_idx = self.vector_instance.find_index_in_vector(str(primer3output[f"PRIMER_LEFT_{index}_SEQUENCE"]))
             fw_primer = Primer()
-            fw_primer.name = "fw_" + eblock.name
+            fw_primer.name = "fw_" + DNABlock.name
             fw_primer.sequence_5to3 = primer3output[f"PRIMER_LEFT_{index}_SEQUENCE"]
             fw_primer.is_forward = True
             fw_primer.idx_start = start_idx
@@ -301,7 +333,7 @@ class DesignPrimers:
             tmpseq = seq.NucleotideSequence(str(primer3output[f'PRIMER_RIGHT_{index}_SEQUENCE'])).complement().reverse()
             start_idx, end_idx = self.vector_instance.find_index_in_vector(str(tmpseq))
             rv_primer = Primer()
-            rv_primer.name = "rv_" + eblock.name
+            rv_primer.name = "rv_" + DNABlock.name
             rv_primer.sequence_5to3 = primer3output[f'PRIMER_RIGHT_{index}_SEQUENCE']
             rv_primer.is_reverse = True
             rv_primer.idx_start = start_idx
@@ -314,7 +346,7 @@ class DesignPrimers:
             if direction == 'forward':
                 start_idx, end_idx = self.vector_instance.find_index_in_vector(str(primer3output[f'PRIMER_LEFT_{index}_SEQUENCE']))
                 seq_primer = Primer()
-                seq_primer.name = "seq_fw_" + eblock.name
+                seq_primer.name = "seq_fw_" + DNABlock.name
                 seq_primer.sequence_5to3 = primer3output[f'PRIMER_LEFT_{index}_SEQUENCE']
                 seq_primer.is_forward = True
                 seq_primer.idx_start = start_idx
@@ -325,7 +357,7 @@ class DesignPrimers:
             elif direction == 'reverse':
                 start_idx, end_idx = self.vector_instance.find_index_in_vector(str(primer3output[f'PRIMER_RIGHT_{index}_SEQUENCE']))
                 seq_primer = Primer()
-                seq_primer.name = "seq_rv_" + eblock.name
+                seq_primer.name = "seq_rv_" + DNABlock.name
                 seq_primer.sequence_5to3 = primer3output[f'PRIMER_RIGHT_{index}_SEQUENCE']
                 seq_primer.is_reverse = True
                 seq_primer.idx_start = start_idx
@@ -341,9 +373,6 @@ class DesignPrimers:
         left = fw_range_start
         right = rv_range_start 
         
-        # TODO Import settings from file
-
-
         while True:
             try:
                 result = primer3.bindings.design_primers(seq_args=Primer3Config.seq_args_primer_pair(sequence,
@@ -365,10 +394,7 @@ class DesignPrimers:
                     raise ValueError("Primer pair not found. Try using less strict settings.")
 
     def add_primers_to_genbank_file(self, genbank_file, primer):
-        """
-        This function saves primer data to an existing GenBank file
-        """
-        # TODO : Check direction of the primer in benchling and see whether this is correct
+        """This function saves primer data to an existing GenBank file."""
         seq_record = SeqIO.read(genbank_file, "genbank")
         feature = None
         site = {}
@@ -410,9 +436,9 @@ class DesignPrimers:
         SeqIO.write(seq_record, genbank_file, "genbank")
         
     def check_overlap(self, min_searchrange=7):
-        min_oh = self.eblocks_design_instance.min_overlap - self.minimum_overhang
+        min_oh = self.DNABlocks_design_instance.min_overlap - self.minimum_overhang
         if min_oh <= 0:
-            raise ValueError("Minimum overhang is smaller then eblocks overlap.")
+            raise ValueError("Minimum overhang is smaller then DNABlocks overlap.")
         elif min_oh < min_searchrange:
             print(f"Minimum overlap is smaller than {min_searchrange}. This might lead to difficulty in finding primers.")
         else:
